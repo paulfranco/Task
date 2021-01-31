073fbc7fa58ea4850d86cb0dacd87fbe1deb53f4
Paul Franco <mail@paulfran.co>
2021-01-31 04:10:44 -0800

    setup data binding

--- app/src/main/java/co/paulfran/task/GroupsActivity.kt
+++ app/src/main/java/co/paulfran/task/GroupsActivity.kt
@@ -2,10 +2,18 @@ package co.paulfran.task
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import co.paulfran.task.databinding.ActivityGroupsBinding
 
 class GroupsActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityGroupsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_groups)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_groups)
+
+        
+
     }
 }
