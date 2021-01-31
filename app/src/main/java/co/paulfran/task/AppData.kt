84fe978d08d03bef6ca6e736932e0d969810afad
Paul Franco <mail@paulfran.co>
2021-01-31 02:53:37 -0800

    data classes and initial data

---
+++ app/src/main/java/co/paulfran/task/AppData.kt
@@ -0,0 +1,20 @@
+package co.paulfran.task
+
+class AppData {
+
+    companion object DataHolder {
+        var groups: MutableList<Group> = mutableListOf()
+
+        fun initialize() {
+            val item1 = Item("Bread", false)
+            val item2 = Item("Bread1", false)
+            val item3 = Item("Bread2", false)
+            val item4 = Item("Bread3", false)
+
+            val group1 = Group("Home", mutableListOf(item1, item2))
+            val group2 = Group("Office", mutableListOf(item3, item4))
+
+            groups = mutableListOf(group1, group2)
+        }
+    }
+}
---
+++ app/src/main/java/co/paulfran/task/ItemsActivity.kt
@@ -0,0 +1,11 @@
+package co.paulfran.task
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_items)
+    }
+}
