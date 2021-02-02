1a6257eb702eb82d90131b2073e52b38a540af48
Paul Franco <mail@paulfran.co>
2021-02-01 16:27:04 -0800

    pass data to items activity and set up ability to navigate back

--- app/src/main/java/co/paulfran/task/ItemsActivity.kt
+++ app/src/main/java/co/paulfran/task/ItemsActivity.kt
@@ -1,11 +1,33 @@
 package co.paulfran.task
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import co.paulfran.task.databinding.ActivityItemsBinding
 
 class ItemsActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityItemsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_items)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_items)
+
+
+        var selectedIndex = intent.getIntExtra("groupIndex", 0)
+        var thisGroup = AppData.groups[selectedIndex]
+
+        binding.toolbarTitle.text = thisGroup.name
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
